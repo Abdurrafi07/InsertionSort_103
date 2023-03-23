69847d1321b5ebe29aaebdee8c09aa7b4717ebb8
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-03-23 17:26:41 +0700

    menambahkan fungsi perulangan for

--- InsertionSort_103/InsertionSort_103.cpp
+++ InsertionSort_103/InsertionSort_103.cpp
@@ -11,9 +11,22 @@ void input()
         cout << "Masukkan Jumlah Data Array : ";        // Membuat inputan Jumlah Elemen Array
         cin >> n;                                       // Memanggil variable inputan n
         if (n <= 20)
-        {
-
+        {   // Membuat kondisi n tidak lebih dari 20 
+            break;
         }
+        else
+        {
+            cout << "n\Array yang anda masukkan maksimal 20 elemen. \n";
+        }
+    }
+    cout << endl;                                       // Membuat jarak per baris program
+    cout << "======================" << endl;           // Membuat tampilan susun data element array
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
+
+    for (int i = 0; i < n; i++)     // Menggunakan perulangan for untuk menyimpan data pada array
+    {
+
     }
 }
 
