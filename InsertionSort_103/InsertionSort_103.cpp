1924661c8ce46f118b9eef07bb8c055d71c15b28
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-03-23 18:42:52 +0700

    Menambahkan fungsi for untuk looping nilai j dimulai dari 0 hingga n-1

--- InsertionSort_103/InsertionSort_103.cpp
+++ InsertionSort_103/InsertionSort_103.cpp
@@ -62,7 +62,16 @@ void insertionsort()
 
 void display()
 {
+    cout << endl;                  // Output baris kosong
+    cout << "Total Element Movement = " << n - 1 << endl;       // cout movement elemen
+    cout << "\n===============================" << endl;        // Output ke layar
+    cout << "Element Array yang telah tersusun" << endl;        // Output ke layar
+    cout << "===============================" << endl;          // Output ke layar
 
+    for (int j = 0; j < n; j++)
+    {                             // Looping dengan j dimulai dari 0 hingga n-1
+
+    }
 }
 
 
